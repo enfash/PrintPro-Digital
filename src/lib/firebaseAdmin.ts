eb076be7716da51ecce27a21607499093bb23385
Elijah Fasugba <elijahfasugba@gmail.com>
2025-12-06 16:47:18 +0000

    ## Error Type Console Error
    ## Error Message
    Failed to upload file

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -9,7 +9,7 @@ if (!admin.apps.length) {
                 clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
                 privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
             }),
-            storageBucket: storageBucket.replace('gs://', ''),
+            storageBucket: storageBucket,
         });
     } catch (error: any) {
         console.error('Firebase admin initialization error', error.stack);
