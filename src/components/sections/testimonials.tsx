88728a67c4ecbfafbd29727bfb410ce8111fd0a9
Elijah Fasugba <elijahfasugba@gmail.com>
2025-12-06 05:02:56 +0000

    container mx-auto px-6 (_for element div_)

--- src/components/sections/testimonials.tsx
+++ src/components/sections/testimonials.tsx
@@ -39,7 +39,7 @@ export default function Testimonials() {
 
   return (
     <section id="testimonials" className="w-full py-12 md:py-24 lg:py-32">
-      <div className="container px-4 md:px-6">
+      <div className="container mx-auto px-6">
         <div className="flex flex-col items-center justify-center space-y-4 text-center">
           <div className="space-y-2">
             <div className="inline-block rounded-lg bg-muted px-3 py-1 text-sm">Client Feedback</div>
