95c6873cb02fa507bacfb0f347465ddef91dedd6
Elijah Fasugba <elijahfasugba@gmail.com>
2025-12-11 10:49:17 +0000

    1. Paste this code as high in the <head> of the page as possible: <!-- G

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 
 import type {Metadata} from 'next';
+import Script from 'next/script';
 import './globals.css';
 import { cn } from '@/lib/utils';
 import { Toaster } from "@/components/ui/toaster"
@@ -37,6 +38,13 @@ export default function RootLayout({
   return (
     <html lang="en" className="!scroll-smooth" suppressHydrationWarning>
       <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-PF8CQGCX');`}
+        </Script>
         <link rel="canonical" href="https://bomedia.ng/" />
         
         {/* Favicon */}
@@ -188,6 +196,14 @@ export default function RootLayout({
 ` }} />
       </head>
       <body className={cn("bg-slate-50 text-slate-900 antialiased selection:bg-primary-200 selection:text-primary-900", "font-body")} suppressHydrationWarning>
+        <noscript>
+            <iframe
+                src="https://www.googletagmanager.com/ns.html?id=GTM-PF8CQGCX"
+                height="0"
+                width="0"
+                style={{ display: 'none', visibility: 'hidden' }}
+            ></iframe>
+        </noscript>
         <FirebaseErrorListener />
         {children}
         <Toaster />
