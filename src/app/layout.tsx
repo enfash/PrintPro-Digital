872150f207bdb4fe89be36a3522a732accd65ca5
Elijah Fasugba <elijahfasugba@gmail.com>
2025-12-19 18:11:19 +0000

    contentScript.js:2 wle:content:translations: Started initialization. [Vi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="!scroll-smooth" suppressHydrationWarning>
+    <html lang="en" className="!scroll-smooth" suppressHydrationWarning data-scroll-behavior="smooth">
       <head>
         <link rel="canonical" href="https://bomedia.com.ng/" />
         
